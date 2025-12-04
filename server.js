1af16cc28cdb0ce209533648c8e1effe31678652
〘 sᴀᴛʜᴀɴɪᴄ 〙 <sathanic498@gmail.com>
2025-12-04 23:53:38 +0530

    A R S H I K A  -  M D

--- server.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Arshika-MD Bot Running Successfully 🚀");
-});
-
-app.listen(3000, () => console.log("Server Alive"));
